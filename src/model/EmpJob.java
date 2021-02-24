a707fd9c010d004b6e80a170b25419263ba8a082
hp <hp@DESKTOP-JE0C1K3>
2021-02-24 22:53:28 +0530

    First commit

---
+++ src/model/EmpJob.java
@@ -0,0 +1,71 @@
+package model;
+
+public class EmpJob {
+	private int ejId;
+	private int employeeId;
+	private int jobId;
+	private String recruited;
+	
+	//Default Constructor
+	
+	public EmpJob() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	//parameterized constructor method will all fields
+	
+	public EmpJob(int ejId, int employeeId, int jobId, String recruited) {
+		super();
+		this.ejId = ejId;
+		this.employeeId = employeeId;
+		this.jobId = jobId;
+		this.recruited = recruited;
+	}
+	
+	//parameterized constructor method without EJId fields
+	
+	public EmpJob(int employeeId, int jobId, String recruited) {
+		super();
+		this.employeeId = employeeId;
+		this.jobId = jobId;
+		this.recruited = recruited;
+	}
+	
+	//getter and setter methods
+	
+	public int getEjId() {
+		return ejId;
+	}
+	public void setEjId(int ejId) {
+		this.ejId = ejId;
+	}
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public int getJobId() {
+		return jobId;
+	}
+	public void setJobId(int jobId) {
+		this.jobId = jobId;
+	}
+	public String getRecruited() {
+		return recruited;
+	}
+	public void setRecruited(String recruited) {
+		this.recruited = recruited;
+	}
+	
+	//to string method
+	
+	@Override
+	public String toString() {
+		return "EmpJob [ejId=" + ejId + ", employeeId=" + employeeId + ", jobId=" + jobId + ", recruited=" + recruited
+				+ "]";
+	}
+	
+	
+}
